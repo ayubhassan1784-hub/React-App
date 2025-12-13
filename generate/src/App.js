fcd096d856cb201c5112ee2c987cc643096fbe4c
ayubhassan1784-hub <ayubhassan1784@gmail.com>
2025-12-13 10:06:31 +0300

    Update1

--- generate/src/App.js
+++ generate/src/App.js
@@ -5,15 +5,26 @@ import CreatePost from "./components/CreatePost";
 
 const App = () => {
     const [user, setUser] = useState("ayub");
+    const [posts, setPosts] = useState([]);
     useEffect(() => {
         document.title = user ? `${user}` : "Please Login";
     }, [user])
     if (!user) return <Login setUser={setUser} />
     return (
         <>
-        <H1>Generate</H1>
+        <h1>Generate</h1>
         <Header user={user} setUser={setUser} />
-        <CreatePost />
+        <CreatePost posts={posts} setPosts={setPosts} />
+        {
+            posts.map(
+                post => (
+                    <div>
+                        <h1>{post.content}</h1>
+                        <img src={URL.createObjectURL(post.image)} alt="" style={{width: "300px"}} />
+                    </div>
+                )
+            )
+        }
         </>
     );
 };
--- generate/src/components/CreatePost.jsx
+++ generate/src/components/CreatePost.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 
-const CreatePost = () => {
+const CreatePost = ({posts, setPosts}) => {
     const [content, setContent] = useState("");
     const [image, setImage] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // console.log(content, image);
+        const newPost = {content, image};
+        setPosts([newPost, ...posts]);
+    }
     return (
         <div>
             <h1>Create Post</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Post Content"
                 onChange={(e)=>setContent(e.target.value)} />
                 <input type="file" 
                 onChange={(e)=> setImage(e.target.files[0])} />
                 <button type="submit">Submit</button>
             </form>
-            <p>{content}</p>
-            {image && <img src={URL.createObjectURL(image)} alt="" 
-            style={{width: "300px"}} />}
+            {/* <p>{content}</p> */}
+            {/* {image && <img src={URL.createObjectURL(image)} alt="" style={{width: "300px"}} />} */}
         </div>
     )
 }

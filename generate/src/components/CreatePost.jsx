bc3752da0f0361ad7af33165a2bf3b5098ad4fe4
ayubhassan1784-hub <ayubhassan1784@gmail.com>
2025-12-13 17:02:28 +0300

    Update CreatePost.jsx

--- generate/src/components/CreatePost.jsx
+++ generate/src/components/CreatePost.jsx
@@ -1,23 +1,28 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const CreatePost = ({posts, setPosts}) => {
     const [content, setContent] = useState("");
     const [image, setImage] = useState(null);
+    const imageRef = useRef("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
         // console.log(content, image);
         const newPost = {content, image};
         setPosts([newPost, ...posts]);
+        setContent('');
+        imageRef.current.value = "";
     }
     return (
         <div>
             <h1>Create Post</h1>
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder="Post Content"
-                onChange={(e)=>setContent(e.target.value)} />
+                onChange={(e)=>setContent(e.target.value)} 
+                value={content} />
                 <input type="file" 
-                onChange={(e)=> setImage(e.target.files[0])} />
+                onChange={(e)=> setImage(e.target.files[0])} 
+                ref={imageRef} />
                 <button type="submit">Submit</button>
             </form>
             {/* <p>{content}</p> */}

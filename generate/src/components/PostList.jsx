72f9364bbbcc319535f00d7764249a7df8cd9c37
ayubhassan1784-hub <ayubhassan1784@gmail.com>
2025-12-23 16:11:00 +0300

    Update PostList.jsx

--- generate/src/components/PostList.jsx
+++ generate/src/components/PostList.jsx
@@ -1,6 +1,20 @@
-const PostList = () => {
-    return (
-        <>PostList</>
-    )
-}
+const PostList = ({ posts }) => {
+  return (
+    <>
+      {posts.map((post, index) => (
+        <div key={index}>
+          <h1>{post.content}</h1>
+          {post.image && (
+            <img
+              src={URL.createObjectURL(post.image)}
+              alt=""
+              style={{ width: "300px" }}
+            />
+          )}
+        </div>
+      ))}
+    </>
+  );
+};
+
 export default PostList;
